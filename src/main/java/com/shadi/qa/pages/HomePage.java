721c2798f00a5a8cabd39a2504688801ae311bd9
Test-QA-Automation <testqaforautomation@gmail.com>
2021-11-17 10:56:07 +0530

    changes updated

--- src/main/java/com/shadi/qa/pages/HomePage.java
+++ src/main/java/com/shadi/qa/pages/HomePage.java
@@ -8,27 +8,14 @@ import org.openqa.selenium.support.PageFactory;
 import com.shadi.qa.base.TestBase;
 
 public class HomePage extends TestBase{
-//	RegistrationProfilePage registrationProfilePage = new RegistrationProfilePage();
 	
-	// object identification
-//	@FindBy(xpath="(//button[@type='button'])[1]")
-//	WebElement letsBeginBtn;
-	
-	// initializing web elements
-//	HomePage(){
-//		PageFactory.initElements(driver, this);
-//	}
-//	
 	// defining properties
 	public static void clickOnLetsBeginBtn() throws Exception {
 		String clickOnBegin = "(//button[@type='button'])[1]";
 		Thread.sleep(10000);
 		driver.findElement(By.xpath(clickOnBegin)).click();
 		Thread.sleep(5000);
-//		registrationProfilePage.passingEmailID("test@test.com");
-//		registrationProfilePage.enteringPassword("12345");
-//		letsBeginBtn.click();
-//		return new RegistrationProfilePage();
+
 	}
 
 }
